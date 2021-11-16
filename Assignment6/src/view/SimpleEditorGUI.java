48deb0f4d41524bb6a63f36becb31bdadd6d68d7
matt-l-01 <matthewstephen01@gmail.com>
2021-11-16 15:47:12 -0500

    New

---
+++ Assignment6/src/view/SimpleEditorGUI.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Description of class goes here.
+ *
+ * @author Matthew Love
+ */
+public class SimpleEditorGUI extends JFrame implements ImageEditorView {
+
+
+  @Override
+  public void renderMessage(String message) throws IllegalStateException {
+
+  }
+}
