aee7d1cc0c8a8f39ea6df88e5d63d876a6f67c07
matt-l-01 <matthewstephen01@gmail.com>
2021-12-02 17:20:26 -0500

    New

--- Assignment7/src/controller/command/Mosaic.java
+++ Assignment7/src/controller/command/Mosaic.java
@@ -8,11 +8,29 @@ import controller.ImageManager;
  * @author Matthew Love
  */
 public class Mosaic extends AbstractImageCommand {
-  public Mosaic(ImageManager model) throws IllegalArgumentException {
+  int seeds;
+  public Mosaic(String[] args, ImageManager model) throws IllegalArgumentException {
     super(model);
+    boolean seedsFound = false;
+    if (args == null) {
+      throw new IllegalArgumentException("Arguments may not be null.");
+    }
+    for (String arg : args) {
+      try {
+        this.seeds = Integer.parseInt(arg);
+        seedsFound = true;
+        return;
+      } catch (NumberFormatException ignored) {
+      }
+    }
+
+    if (!seedsFound) {
+      throw new IllegalArgumentException("Must specify an amount of seeds.");
+    }
   }
 
   @Override
   public void execute(String inputName, String resultName) {
+    model.mosaic(this.seeds, inputName, resultName);
   }
 }
